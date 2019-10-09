1b6da60f545214b9a5c415d0fcaa743c3b44f35a
niku <niku@niku.name>
2019-10-09 19:39:57 +0900

    Write README

--- lib/jwk_store.ex
+++ lib/jwk_store.ex
@@ -3,21 +3,11 @@ defmodule JWKStore do
   Documentation for JWKStore.
   """
 
+  use Supervisor
   alias JWKStore.{Storage, Updater}
 
   @storage JWKStore.Storage
   @updater JWKStore.Updater
-
-  @spec start_link(URI.t()) :: {:error, any} | {:ok, pid}
-  def start_link(%URI{} = url) do
-    children = [
-      {@storage, name: @storage},
-      {@updater, {@storage, url, name: @updater}}
-    ]
-
-    Supervisor.start_link(children, strategy: :one_for_one, name: __MODULE__)
-  end
-
   @spec lookup(binary) :: nil | map
   def lookup(key) when is_binary(key) do
     Storage.lookup(@storage, key)
@@ -26,5 +16,20 @@ defmodule JWKStore do
   @spec get_millisec_to_next_update :: non_neg_integer
   def get_millisec_to_next_update do
     Updater.get_millisec_to_next_update(@updater)
+  end
+
+  @spec start_link(URI.t()) :: {:error, any} | {:ok, pid}
+  def start_link(%URI{} = uri) do
+    Supervisor.start_link(__MODULE__, uri, name: __MODULE__)
+  end
+
+  @impl Supervisor
+  def(init(%URI{} = uri)) do
+    children = [
+      {@storage, name: @storage},
+      {@updater, {@storage, uri, name: @updater}}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end
